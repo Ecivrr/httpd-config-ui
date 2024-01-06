d9f6938985627d9e122f52c1e1a1a4399fb5400b
Ecivrr <matous401@gmail.com>
2024-01-06 16:59:40 +0100

    HTTPD config worikng

--- config_script.sh
+++ config_script.sh
@@ -31,11 +31,23 @@ while [ "${EXITSTATUS}" == "continue" ]; do
 		input "Configure the httpd.conf file" "Input the server name or IP address."
 		input_data "SERVERNAME"
 
-#		echo "${LISTENIP}"
-#		echo "${ADMINEMAIL}"
-#		echo "${SERVERNAME}"
+		cp /opt/httpd_config_ui/templates/httpd_template /tmp/httpd.conf
+		sed -i "s/_LISTENIP_/${LISTENIP}/g" /tmp/httpd.conf
+		sed -i "s/_ADMINMAIL_/${ADMINEMAIL}/g" /tmp/httpd.conf
+		sed -i "s/_SERVERNAME_/${SERVERNAME}/g" /tmp/httpd.conf
 
-#		change template using sed
+		if [ -e "/etc/httpd/conf/httpd.conf" ]; then
+			DIFF=$(diff /tmp/httpd.conf /etc/httpd/conf/httpd.conf)
+			if [ "${DIFF}" ]; then
+				DATE=$(date +%y%m%d%H%M%S)
+				mv /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.backup_${DATE}
+				mv /tmp/httpd.conf /etc/httpd/conf/httpd.conf
+			else
+				rm -f /tmp/httpd.conf
+			fi
+		else
+			mv /tmp/httpd.conf /etc/httpd/conf/httpd.conf
+		fi
 		exit 0
 
 	elif [ "${CONFIG_MENU}" == "VIRTUAL HOSTS" ]; then
